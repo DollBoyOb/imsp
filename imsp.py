b538685c750f61e49ef4927d83e45b71055d8a50
DollBoyOb <165464616+DollBoyOb@users.noreply.github.com>
2024-11-15 18:45:43 +0500

    Making code more understandable

--- imsp.py
+++ imsp.py
@@ -24,16 +24,24 @@ while n!=len(f):
                 heap[int(com[1])] = heap[int(com[3])][heap[int(com[4])]]
             elif int(com[2]) == 2:
                 heap[int(com[1])] = heap[int(com[3])]
-            else:
-                heap[int(com[1])] = [chr(int("".join(com[3:])[::-1])),int(com[3])][int(com[2])] 
+            elif int(com[2]) == 1:
+                heap[int(com[1])] = int(com[3])
+            elif int(com[2]) == 0:
+                heap[int(com[1])] = chr(int("".join(com[3:])[::-1]))
         case "2":
             ops = ["+", "-", "*", "/", "%"]            
             eval_str = str(heap[int(com[1])])+ops[int(com[2])]+str(heap[int(com[3])])
-            if [type(heap[int(com[1])]), type(heap[int(com[3])])] == [str,str]: eval_str = str(f" '{heap[int(com[1])]}'{ops[int(com[2])]}'{heap[int(com[3])]}' ")
+            
+            # Checks if both cells is string
+            if [type(heap[int(com[1])]), type(heap[int(com[3])])] == [str,str]: 
+                eval_str = str(f" '{heap[int(com[1])]}'{ops[int(com[2])]}'{heap[int(com[3])]}' ")
+            
             heap[int(com[4])] = eval(eval_str)
         case "3":
             eqs = ["==", "<", ">", "!="]
-            n+=(1-eval(str(f" '{heap[int(com[1])]}'{eqs[int(com[2])]}'{heap[int(com[3])]}' ")))
+            # Example: 1-eval("30<10") = 1-False = 1
+            # n is a pointer, and in this example, pointer just skips next line of code
+            n+=(1-eval(str(f" '{heap[int(com[1])]}'{eqs[int(com[2])]}'{heap[int(com[3])]}' "))) 
         case "4":
             n = int(com[1])-1
         case "5": break
